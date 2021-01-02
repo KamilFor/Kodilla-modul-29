26251229068c8e044ebf762e6e96c0ace6e35e79
Kamil Fornalski <kester.kamil@gmail.com>
2021-01-02 16:32:40 +0100

    29.3 DONE

--- server.js
+++ server.js
@@ -17,39 +17,46 @@ mongoClient.connect(
       // MongoDB
       // import Data from MongoDB database
       const db = client.db('companyDB');
-
-      db.collection('employees').find({ department: 'IT' }, (err, data) => {
-        if (!err) {
-          data.each((error, employee) => {
-            console.log(employee);
-          });
-        }
-        //do something with data
-        // Adding Data to Departments
-        // db.collection('departments').insertOne({ name: 'Management' }, (err) => {
-        //   if (err) console.log('err');
-        // });
-        console.log('do sth');
-      });
-
-      // Express usage
       const app = express();
 
       app.use(cors());
       app.use(express.json());
       app.use(express.urlencoded({ extended: false }));
 
-      app.use('/api', employeesRoutes);
+      app.use((req, res, next) => {
+        req.db = db;
+        next();
+      });
+
       app.use('/api', departmentsRoutes);
+      app.use('/api', employeesRoutes);
       app.use('/api', productsRoutes);
 
+      // Not found
       app.use((req, res) => {
         res.status(404).send({ message: 'Not found...' });
       });
 
+      // Server;
       app.listen('8000', () => {
         console.log('Server is running on port: 8000');
       });
+
+      // DB collection
+      //   db.collection('employees').find({ department: 'IT' }, (err, data) => {
+      //     if (!err) {
+      //       data.each((error, employee) => {
+      //         console.log(employee);
+      //       });
+      //     }
+
+      //     // Adding Data to Departments
+      //     // db.collection('departments').insertOne({ name: 'Management' }, (err) => {
+      //     //   if (err) console.log('err');
+      //     // });
+      //   });
+
+      // Express usage
     }
   }
 );
