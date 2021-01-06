d72b1c2e3d656e0433227a012f456fae9ccf6d3f
Kamil Fornalski <kester.kamil@gmail.com>
2021-01-06 19:17:54 +0100

    30.2

---
+++ models/test/employees.test.js
@@ -0,0 +1,59 @@
+const Emplyoees = require('../department.model.js');
+const expect = require('chai').expect;
+
+describe('Emplyoees', () => {
+  it('should throw an error if no "name" arg', () => {
+    const employe = new Emplyoees({}); // create new Department, but don't set `name` attr value
+
+    employe.validate((err) => {
+      expect(err.errors.name).to.exist;
+    });
+  });
+  // Test 2
+  it('should throw an error if one of the props is not a string', () => {
+    const cases = [{}, []];
+    for (let firstName of cases) {
+      const employe = new Emplyoees({ firstName });
+
+      employe.validate((err) => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+    for (let lastName of cases) {
+      const employe = new Emplyoees({ lastName });
+
+      employe.validate((err) => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+    for (let department of cases) {
+      const employe = new Emplyoees({ department });
+
+      employe.validate((err) => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+  // Test 3
+  it('should throw an error if "name" is too short or too long', () => {
+    const cases = ['Abc', 'abcd', 'Lorem Ipsum, Lorem Ip']; // we test various cases, some of them are too short, some of them are too long
+    for (let firstName of cases) {
+      const employe = new Emplyoees({ firstName, lastName });
+
+      employe.validate((err) => {
+        expect(err.errors.name).to.exist;
+      });
+    }
+  });
+  //Test 4
+  it('should not throw an error if "name" is okay', () => {
+    const cases = ['Management', 'Human Resources'];
+    for (let firstName of cases) {
+      const dep = new Emplyoees({ firstName });
+
+      dep.validate((err) => {
+        expect(err).to.not.exist;
+      });
+    }
+  });
+});
